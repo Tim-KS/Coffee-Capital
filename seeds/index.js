1fa264893e7d41931da86e04ac24e2a3302826a3
Sean Wallace <sean.wallace.australia@gmail.com>
2022-11-05 00:59:54 +1100

    add session seed data

--- seeds/index.js
+++ seeds/index.js
@@ -1,12 +1,13 @@
 const sequelize = require('../config/connection');
 // const { User, Order, OrderItem, Category, Product } = require('../models');
-const { User, Category, Product, OrderDetail, OrderItem } = require('../models');
+const { User, Category, Product, OrderDetail, OrderItem, Session } = require('../models');
 
 const seedUsers = require('./user-seeds');
 const seedCategories = require('./category-seeds');
 const seedProducts = require('./product-seeds');
 const seedOrderDetails = require('./order-detail-seeds');
 const seedOrderItems = require('./order-item-seeds');
+const seedSessions = require('./session-seeds');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
@@ -26,6 +27,9 @@ const seedAll = async () => {
   
   await seedOrderItems();
   console.log('\n----- ORDER ITEMS SEEDED -----\n');
+
+  await seedSessions();
+  console.log('\n----- SESSIONS SEEDED -----\n');
 
   process.exit(0);
 };
