9a5141f8d54a854c1f54840bf93fc825a2a8c881
Sean Wallace <sean.wallace.australia@gmail.com>
2022-11-04 19:46:20 +1100

    add password to user.js eslintrc/ignore updated/added

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -3,49 +3,49 @@ const { User } = require('../models');
 const userData = [
   {
     email: 'admin@coffeecapital.com',
-    password: '',
+    password: 'Password1!',
     first_name: 'Adam',
     last_name: 'Berry',
     is_admin: true
   },
   {
     email: 'ben@abc.com',
-    password: '',
+    password: 'Password2!',
     first_name: 'Ben',
     last_name: 'Chalmers',
     is_admin: false
   },
   {
     email: 'cathy@abc.com',
-    password: '',
+    password: 'Password3!',
     first_name: 'Cathy',
     last_name: 'Davidson',
     is_admin: false
   },
   {
     email: 'sean.wallace.australia@gmail.com',
-    password: '',
+    password: 'Password4!',
     first_name: 'Sean',
     last_name: 'Wallace',
     is_admin: false
   },
   {
     email: 'diane@mycompany.com',
-    password: '',
+    password: 'Password5!',
     first_name: 'Diane',
     last_name: 'Enterprise',
     is_admin: false
   },
   {
     email: 'ed@mycompany.com',
-    password: '',
+    password: 'Password6!',
     first_name: 'Ed',
     last_name: 'Franklin',
     is_admin: false
   },
   {
     email: 'fiona@mycompany.com',
-    password: '',
+    password: 'Password7!',
     first_name: 'Fiona',
     last_name: 'Griffith',
     is_admin: false
