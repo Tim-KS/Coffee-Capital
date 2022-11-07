9fcc6f94516f0dd384257ae7abce9c07fdefb502
Sean Wallace <sean.wallace.australia@gmail.com>
2022-11-07 20:45:28 +1100

    mainlogin draft

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,9 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+console.log('controllers/index.js')
+
+router.use('/', index);
 router.use('/api', apiRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,57 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+console.log('11111111111111111111111')
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+console.log('222222222222222222222')
+app.use(session(sess));
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// turn on routes
+app.use(express.static(path.join(__dirname, 'public')));
+console.log('333333333333333333333')
 app.use(routes);
 
-// turn on connection to db and server
+
+// Fallback route for when a user attempts to visit routes that don't exist
+// ALWAYS PUT THIS WILDCARD SAFETY NET AT THE END OR NOTHING ELSE WILL EXECUTE!
+// source: 1105-Ins_Query-Params
+app.get('*', (req, res) =>
+  res.send(
+    `INVALID ROUTE @: http://localhost:${PORT}`
+  )
+);
+
+
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now listening on PORT: ${PORT}...`));
+  app.listen(PORT, () => console.log(`Now listening at http://localhost:${PORT}`));
 });
