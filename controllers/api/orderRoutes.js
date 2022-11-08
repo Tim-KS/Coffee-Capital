fa206f7a592890a936bbbb6f081b827ad6fb6d13
Bhavika Mevada <109460560+bmevada@users.noreply.github.com>
2022-11-08 20:33:53 +1100

    order routes needs to be ammended

---
+++ controllers/api/orderRoutes.js
@@ -0,0 +1,82 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { User, Order } = require('../../models');
+const withAuth = require('../utils/auth');
+
+// POST request for the oder
+
+router.post('/', async (req, res) => {
+  try {
+    const newOrder = await Order.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newOrder);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const orderData = await Order.destroy({
+      where: {const router = require('express').Router();
+      const sequelize = require('../config/connection');
+      const { User, Order } = require('../../models');
+      const withAuth = require('../utils/auth');
+      
+      // POST request for the oder
+      
+      router.post('/', async (req, res) => {
+        try {
+          const newOrder = await Order.create({
+            ...req.body,
+            user_id: req.session.user_id,
+          });
+      
+          res.status(200).json(newOrder);
+        } catch (err) {
+          res.status(400).json(err);
+        }
+      });
+      
+      router.delete('/:id', async (req, res) => {
+        try {
+          const orderData = await Order.destroy({
+            where: {
+              id: req.params.id,
+              user_id: req.session.user_id,
+            },
+          });
+      
+          if (!orderData) {
+            res.status(404).json({ message: 'No order found with this id!' });
+            return;
+          }
+      
+          res.status(200).json(orderData);
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      });
+      
+      module.exports = router;
+      
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!orderData) {
+      res.status(404).json({ message: 'No order found with this id!' });
+      return;
+    }
+
+    res.status(200).json(orderData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
