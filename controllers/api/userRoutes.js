0ff6052d12a72907e7c42155dd3fd62f1a55b69d
Bhavika Mevada <109460560+bmevada@users.noreply.github.com>
2022-11-10 19:57:34 +1100

    working connection

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-const sequelize = require('../config/connection');
+// const sequelize = require('./config/connection');
 const { User, Order} = require('../../models');
-const withAuth = require('../utils/auth');
+const withAuth = require('../../utils/auth');
 
 // Login
 
